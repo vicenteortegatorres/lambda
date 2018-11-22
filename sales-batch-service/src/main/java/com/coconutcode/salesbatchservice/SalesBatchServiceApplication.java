ce471491cbae7eff3756568f63b2577db71f9208
Vicente <vicente.ortega.torres@gmail.com>
2018-11-22 22:22:02 +0100

    sales-batch-service added

---
+++ sales-batch-service/src/main/java/com/coconutcode/salesbatchservice/SalesBatchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.coconutcode.salesbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesBatchServiceApplication.class, args);
+	}
+}
